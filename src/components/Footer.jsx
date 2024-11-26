5329edaad5953de36c74cd6a815ea47e5fa5cccd
Vishnu P <vinventor1999@gmail.com>
2024-11-26 15:48:03 +0530

    Social media

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,72 +9,80 @@ import instagram from "../assets/instagram.png";
 
 const Footer = () => {
   return (
-   <div>
-    <div className="grid grid-cols-4">
+   <div className='bg-blue-100 pt-5'>
+    <div className="grid grid-cols-4 mx-10 ">
 
       <div>
-         <h1>lo<span className="text-[#4691F2]">go</span></h1>
-         <p>We are company name Car Rentals,an online cab booking aggregator</p>
-         <div className="flex bg-blue h-[100px]">
-            <img src={facebook} alt="" />
-            <img src={twitter} alt="" />
-            <img src={youtube} alt="" />
-            <img src={instagram} alt="" />
+         <h1 className='text-xl font-bold hover:scale-95 duration-300 ease-in-out'>lo<span className="text-[#4691F2]">go</span></h1>
+         <p className='hover:scale-95 duration-300 ease-in-out mt-3'>We are company name Car Rentals,an online cab booking aggregator</p>
+         <div className="flex gap-2 mt-7">
+            <div className='bg-[#4691F2] rounded-full hover:scale-105 duration-300 ease-in-out p-1'>
+               <img src={facebook} alt="" />
+            </div>
+            <div className='bg-[#4691F2] rounded-full hover:scale-105 duration-300 ease-in-out p-1'>
+               <img src={twitter} alt="" />
+            </div>
+            <div className='bg-[#4691F2] rounded-full hover:scale-105 duration-300 ease-in-out p-1'>
+               <img src={youtube} alt="" />
+            </div>
+            <div className='bg-[#4691F2] rounded-full hover:scale-105 duration-300 ease-in-out p-1'>
+               <img src={instagram} alt="" />
+            </div>
          </div>
       </div>
 
       <div>
-         <h1>Quick Links</h1>
+         <h1 className='font-bold hover:scale-95 duration-300 ease-in-out mb-1'>Quick Links</h1>
          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Serevice</li>
-            <li>Blog</li>
-            <li>Features</li>
-            <li>Contact Us</li>
+            <li className='hover:scale-95 duration-300 ease-in-out'>Home</li>
+            <li className='hover:scale-95 duration-300 ease-in-out'>About Us</li>
+            <li className='hover:scale-95 duration-300 ease-in-out'>Serevice</li>
+            <li className='hover:scale-95 duration-300 ease-in-out'>Blog</li>
+            <li className='hover:scale-95 duration-300 ease-in-out'>Features</li>
+            <li className='hover:scale-95 duration-300 ease-in-out'>Contact Us</li>
          </ul>
       </div>
 
       <div>
-         <h1>Get In Touch</h1>
+         <h1 className='font-bold hover:scale-95 duration-300 ease-in-out mb-1'>Get In Touch</h1>
          <ul>
-            <li>
+            <li className='hover:scale-95 duration-300 ease-in-out'>
                +91 123456789
             </li>
-            <li>
+            <li className='hover:scale-95 duration-300 ease-in-out'>
                company@gmail.com
             </li>
-            <li>company 123 street d block,4th floor</li>
+            <li className='hover:scale-95 duration-300 ease-in-out'>company 123 street d block,4th floor</li>
          </ul>
       </div>
 
       <div>
-         <h1>Follow Us On @instagram</h1>
-         <div className="grid grid-cols-3 gap-1">
-            <div>
-               <img src={footer1} alt="footer1" />
-            </div>
-            <div>
-               <img src={footer2} alt="footer2" />
-            </div>
-            <div>
-               <img src={footer3} alt="footer3" />
-            </div>
-            <div>
-               <img src={footer1} alt="footer1" />
-            </div>
-            <div>
-               <img src={footer2} alt="footer2" />
-            </div>
-            <div>
-               <img src={footer3} alt="footer3" />
-            </div>
-         </div>
-      </div>
+  <h1 className="font-bold hover:scale-95 duration-300 ease-in-out mb-1">Follow Us On @instagram</h1>
+  <div className="grid grid-cols-3 gap-1">
+    <div>
+      <img src={footer1} alt="footer1" className="hover:scale-95 duration-300 ease-in-out aspect-square" />
+    </div>
+    <div>
+      <img src={footer2} alt="footer2" className="hover:scale-95 duration-300 ease-in-out aspect-square" />
+    </div>
+    <div>
+      <img src={footer3} alt="footer3" className="hover:scale-95 duration-300 ease-in-out aspect-square" />
+    </div>
+    <div>
+      <img src={footer1} alt="footer1" className="hover:scale-95 duration-300 ease-in-out aspect-square" />
+    </div>
+    <div>
+      <img src={footer2} alt="footer2" className="hover:scale-95 duration-300 ease-in-out aspect-square" />
+    </div>
+    <div>
+      <img src={footer3} alt="footer3" className="hover:scale-95 duration-300 ease-in-out aspect-square" />
+    </div>
+  </div>
+</div>
 
     </div>
 
-    <h3 className="w-full h-[5vh] bg-[#4691F2] text-white">2024 Developed By Snowfinch Consulting Group
+    <h3 className="w-full h-[10vh] bg-[#4691F2] text-white font-bold text-center mt-5 pt-3">2024 Developed By Snowfinch Consulting Group
     </h3>
 
    </div>
